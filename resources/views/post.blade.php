2597460b27f9f008753181f499757a564f6dc62b
IT Kiosnet <indra@gmail.com>
2023-03-09 17:22:56 +0700

    Database Seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
    <h2 class="tracking-tight text-3xl font-bold mb-3">{{  $post->title  }}</h2>
-   <p>By. Yanto Basna In <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+   <p>By. <a href="#" class="font-medium text-blue-800 dark:text-blue-600 hover:underline">{{ $post->user->name }} </a> In <a href="/categories/{{ $post->category->slug }}" class="font-medium text-blue-800 dark:text-blue-600 hover:underline">{{ $post->category->name }}</a></p>
     <p class="text-justify mb-3 font-light text-gray-500 dark:text-gray-900 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-500 first-letter:mr-3 first-letter:float-left">{!! $post->body !!}</p>
     <a href="/blog" class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline mt-5"> Go Back</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,22 @@
 @extends('partial.main')
 
 @section('container')
-    @foreach ( $post as $posts )
-    <article class="mb-5 mt-4">
-        <h2 class="tracking-tight text-3xl font-bold mb-3">
-            <a href="/blog/{{ $posts->slug }}">{{  $posts->title  }}</a>
-        </h2>
-        <p class="text-2xl mb-3 font-light first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">{{  $posts->excerpt  }}</p>
-    </article>
-    @endforeach
+<div class="text-5xl font-extrabold">
+    <p class="text-4xl font-bold text-gray-900 dark:text-gray-900">POST</p>
+</div>
+    <div class="box-border mt-4 p-4 border-4 border-solid border-rose-500">
+        @foreach ( $post as $posts )
+        <article class="mb-5 mt-4 divide-y divide-blue-400 pb-4">
+            <h2 class="tracking-tight text-3xl font-bold mb-3">
+                <a href="/blog/{{ $posts->slug }}">{{  $posts->title  }}</a>
+            </h2>
+
+            <p class="mb-2 font-bold ">By. <a href="#" class="font-medium text-blue-800 dark:text-blue-600 hover:underline">{{ $posts->user->name }}</a> In <a href="/categories/{{ $posts->category->slug }}" class="font-medium text-blue-800 dark:text-blue-600 hover:underline">{{ $posts->category->name }}</a></p>
+
+            <p class="font-bold text-justify text-lg capitalize text-gray-400 dark:text-gray-900">{{  $posts->excerpt  }}</p>
+
+            <a href="/blog/{{ $posts->slug }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read More...</a>
+        </article>
+        @endforeach
+    </div>
 @endsection
